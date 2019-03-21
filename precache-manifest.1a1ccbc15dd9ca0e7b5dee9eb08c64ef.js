2548811fe75aeffbcf3678ad9d3da435e61c494a
Youngsoo <youngsoo.brain@gmail.com>
2019-03-21 21:30:37 +0900

    Updates

---
+++ precache-manifest.1a1ccbc15dd9ca0e7b5dee9eb08c64ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f8544304b0644c5bcbbfc362e51cb42c",
+    "url": "/cloudoc-test/static/media/static_background.f8544304.jpg"
+  },
+  {
+    "revision": "0fa3fe04edf6c0202970f2088edea9e7",
+    "url": "/cloudoc-test/static/media/google-logo.0fa3fe04.png"
+  },
+  {
+    "revision": "b9a13adb55e9323ff544",
+    "url": "/cloudoc-test/static/js/runtime~main.b9a13adb.js"
+  },
+  {
+    "revision": "6abb2fa44e97e072cb22",
+    "url": "/cloudoc-test/static/js/main.6abb2fa4.chunk.js"
+  },
+  {
+    "revision": "52e6ab4d74618a73081d",
+    "url": "/cloudoc-test/static/js/1.52e6ab4d.chunk.js"
+  },
+  {
+    "revision": "6abb2fa44e97e072cb22",
+    "url": "/cloudoc-test/static/css/main.d43c1799.chunk.css"
+  },
+  {
+    "revision": "1d59d8656844a92ad54c98dd971f65b8",
+    "url": "/cloudoc-test/index.html"
+  }
+];
